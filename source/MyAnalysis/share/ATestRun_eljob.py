12d450c0dc830f9826509dffca366c26c47acbf7
xzf89718 <58804221+xzf89718@users.noreply.github.com>
2021-01-18 11:40:36 +0800

    Add ATestRun_eljob.py

---
+++ source/MyAnalysis/share/ATestRun_eljob.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+# Read the submission directory as a command line argument. You can
+# extend the list of arguments with your private ones later on.
+import optparse
+parser = optparse.OptionParser()
+parser.add_option( '-s', '--submission-dir', dest = 'submission_dir',
+                   action = 'store', type = 'string', default = 'submitDir',
+                   help = 'Submission directory for EventLoop' )
+( options, args ) = parser.parse_args()
+
+# Set up (Py)ROOT.
+import ROOT
+ROOT.xAOD.Init().ignore()
+
+# Set up the sample handler object. See comments from the C++ macro
+# for the details about these lines.
+import os
+sh = ROOT.SH.SampleHandler()
+sh.setMetaString( 'nc_tree', 'CollectionTree' )
+inputFilePath = os.getenv( 'ALRB_TutorialData' ) + '/mc16_13TeV.410470.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad.deriv.DAOD_PHYS.e6337_s3126_r10201_p4172/'
+ROOT.SH.ScanDir().filePattern( 'DAOD_PHYS.21569875._001323.pool.root.1' ).scan( sh, inputFilePath )
+sh.printContent()
+
+# Create an EventLoop job.
+job = ROOT.EL.Job()
+job.sampleHandler( sh )
+job.options().setDouble( ROOT.EL.Job.optMaxEvents, 500 )
+job.options().setString( ROOT.EL.Job.optSubmitDirMode, 'unique-link')
+
+# Create the algorithm's configuration.
+from AnaAlgorithm.DualUseConfig import createAlgorithm
+alg = createAlgorithm ( 'MyxAODAnalysis', 'AnalysisAlg' )
+
+# later on we'll add some configuration options for our algorithm that go here
+
+# Add our algorithm to the job
+job.algsAdd( alg )
+
+# Run the job using the direct driver.
+driver = ROOT.EL.DirectDriver()
+driver.submit( job, options.submission_dir )
